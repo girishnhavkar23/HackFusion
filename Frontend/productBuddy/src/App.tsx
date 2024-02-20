e94070ddd63a44796d138231049dee201132ee24
melbin <melbinkoshy2002@gmail>
2024-02-20 18:13:48 +0530

    seller auth and pages

--- Frontend/productBuddy/src/App.tsx
+++ Frontend/productBuddy/src/App.tsx
@@ -1,59 +1,76 @@
-import { Suspense, useEffect} from 'react'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './App.css'
-import { Toaster } from 'react-hot-toast';
-import SpinnerCircular from './components/ui/SpinnerCircular';
-import React from 'react';
-import { useProfileStore } from './store/store';
+import { Suspense, useEffect } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { Toaster } from "react-hot-toast";
+import SpinnerCircular from "./components/ui/SpinnerCircular";
+import React from "react";
+import { useProfileStore } from "./store/store";
 
-const HomePage = React.lazy(()=>import ('./pages/Home/Home'))
-const Products = React.lazy(()=>import('./pages/Products/Products'))
-const AllProducts = React.lazy(()=>import ('./pages/Products/AllProducts'))
-const ProductPage = React.lazy(()=>import ('./pages/Products/ProductPage'))
+const HomePage = React.lazy(() => import("./pages/Home/Home"));
+const Products = React.lazy(() => import("./pages/Products/Products"));
+const AllProducts = React.lazy(() => import("./pages/Products/AllProducts"));
+const ProductPage = React.lazy(() => import("./pages/Products/ProductPage"));
+const SellerPage = React.lazy(() => import("./pages/Seller/SellerPage"));
+const Signin = React.lazy(() => import("./pages/Auth/SignIn"));
+const Signup = React.lazy(() => import("./pages/Auth/SignUp"));
+
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage/>,
+    path: "/signin",
+    element: <Signin/>,
   },
   {
-    path:"/dash",
-    element: <Products/>,
-    children:[
+    path: "/signup",
+    element: <Signup/>,
+  },
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/dash",
+    element: <Products />,
+    children: [
       {
-        path:"allProducts",
-        element:<AllProducts/>,
+        path: "allProducts",
+        element: <AllProducts />,
       },
-    ]
+    ],
   },
   {
     path: "/product/:productId",
-    element: <ProductPage/>,
+    element: <ProductPage />,
   },
-
-])
+  {
+    path: "/seller",
+    element: <SellerPage/>,
+    children:[
+      {
+        path: "allProducts",
+        element: <AllProducts />,
+      }
+    ]
+  },
+]);
 function App() {
-  const setUser = useProfileStore((state)=>state.setUser)
+  const setUser = useProfileStore((state) => state.setUser);
   useEffect(() => {
     // Check if Profile exists in localStorage
-    const profileData = localStorage.getItem('Profile');
-    console.log(profileData)
+    const profileData = localStorage.getItem("Profile");
+    console.log(profileData);
     if (profileData) {
       // If exists, parse the JSON string and set the user
       const { token, role } = JSON.parse(profileData);
       setUser({ token, role });
     }
   }, [setUser]);
-  
 
   return (
-    <Suspense fallback={<SpinnerCircular/>}>
-       <RouterProvider router={router} />
-       <Toaster />
-      </Suspense>
-  )
+    <Suspense fallback={<SpinnerCircular />}>
+      <RouterProvider router={router} />
+      <Toaster />
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
