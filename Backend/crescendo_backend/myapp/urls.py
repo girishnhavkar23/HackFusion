608182b173c56f12f6df1781a63c497efb05a124
Vedant Pathare <patharevedant@gmail.com>
2024-02-20 13:51:06 +0530

    Added review endpoint

--- Backend/crescendo_backend/myapp/urls.py
+++ Backend/crescendo_backend/myapp/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('products/', views.ProductListView.as_view(), name='products'),
+    path('products/<int:product_id>/reviews/', views.ProductReviewListView.as_view(), name='product_reviews'),
 ]
--- Backend/crescendo_backend/myapp/views.py
+++ Backend/crescendo_backend/myapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 from rest_framework.generics import ListAPIView
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Review
+from .serializers import ProductSerializer, ReviewSerializer
 
 # Create your views here.
 
@@ -13,3 +13,10 @@ def home(request):
 class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ProductReviewListView(ListAPIView):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        product_id = self.kwargs['product_id']
+        return Review.objects.filter(product_id=product_id)
