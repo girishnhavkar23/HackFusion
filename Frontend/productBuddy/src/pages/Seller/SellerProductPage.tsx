6d502f34a3d29cc3890b7d59bb64985bba1989cc
Girish Nhavkar <girishnhavkar44@gmail.com>
2024-02-20 21:55:56 +0530

    updated ui

--- Frontend/productBuddy/src/pages/Seller/SellerProductPage.tsx
+++ Frontend/productBuddy/src/pages/Seller/SellerProductPage.tsx
@@ -13,7 +13,7 @@ const data = {
       label: '# of Votes',
       data: [12, 19, 3, 5, 2, 3],
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 1)',
         'rgba(54, 162, 235, 0.2)',
         'rgba(255, 206, 86, 0.2)',
         'rgba(75, 192, 192, 0.2)',
